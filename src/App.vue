4e1dc1c826cc38a464c25e8b5c39419e17021146
Carolina Gonzalez <c.gonzalezcontrera@gmail.com>
2021-06-03 10:00:10 -0400

    agregando fontawesome

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <h1>Carolina</h1>
+      <font-awesome-icon :icon="['fas','shopping-cart']"/>
 
     </div>
     <router-view />
--- src/main.js
+++ src/main.js
@@ -13,6 +13,14 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 // Vue.use(IconsPlugin)
 
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas);
+Vue.component('font-awesome-icon',FontAwesomeIcon  );
+
 Vue.config.productionTip = false;
 
 new Vue({
